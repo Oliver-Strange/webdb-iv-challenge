4a7c6c764a241fc89c2ea067ebf56006798ff040
Oliver-Strange <preston.burton@gmail.com>
2019-06-13 14:05:22 -0700

    sets up bootstap migration tables to be ran

--- data/migrations/20190613130557_bootstrap.js
+++ data/migrations/20190613130557_bootstrap.js
@@ -1,8 +1,55 @@
-
+// ./data/migrations/timestamp_bootstrap.js
 exports.up = function(knex, Promise) {
-  
+  // the tables must be created in the right order,
+  // tables with FKs are created after the referenced table is
+  return knex.schema
+    .createTable("dishes", tbl => {
+      tbl.increments();
+      tbl
+        .string("name", 128)
+        .notNullable()
+        .unique();
+    })
+    .createTable("recipes", tbl => {
+      tbl.increments();
+      tbl
+        .string("name", 128)
+        .notNullable()
+        .unique();
+      tbl
+        .integer("dish_id")
+        .unsigned()
+        .references("id")
+        .inTable("dishes")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+    })
+    .createTable("ingredients", tbl => {
+      tbl.increments();
+      tbl.string("name", 128).notNullable();
+    })
+    .createTable("recipe_ingredients", tbl => {
+      tbl.increments();
+      tbl
+        .integer("dish_id")
+        .unsigned()
+        .references("id")
+        .inTable("dishes")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+      tbl
+        .integer("ingredients_id")
+        .unsigned()
+        .references("id")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+    });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema
+    .dropTableIfExists("recipe_ingredients")
+    .dropTableIfExists("ingredients")
+    .dropTableIfExists("recipes")
+    .dropTableIfExists("dishes");
 };
